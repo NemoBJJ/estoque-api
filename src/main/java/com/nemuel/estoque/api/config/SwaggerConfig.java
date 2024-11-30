47a455c70619e4fa234692dd401175e645785e10
NemoBJJ <engnfaraujo@gmail.com>
2024-11-30 12:43:13 -0300

    SWAGGER UI FUNCIONANDO -

--- src/main/java/com/nemuel/estoque/api/config/SwaggerConfig.java
+++ src/main/java/com/nemuel/estoque/api/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.nemuel.estoque.api.config;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Estoque API")
                         .version("1.0")
-                        .description("API para gerenciamento de estoque e autenticação com JWT"));
+                        .description("API para gerenciamento de estoque e autenticação com JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
